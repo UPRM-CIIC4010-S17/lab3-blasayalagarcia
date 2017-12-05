24e43cfa50571c21c9d40986ec2d0ef6194cd2d6
4015f2 <4015f2@nispero.ece.uprm.edu>
2017-12-05 11:22:12 -0400

    borders

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -2,11 +2,13 @@ import javax.swing.JFrame;
 
 public class Lab3Exercises {
 	public static void main(String[] args) {
-		JFrame myFrame = new JFrame("Hello, world!");
+		JFrame myFrame = new JFrame("Blas Andres");
 		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		
 		myFrame.setLocation(850,400);
 		myFrame.setSize(200, 200);
+		 MyPanelClass myPanel = new MyPanelClass();
+         myFrame.getContentPane().add(myPanel);
 		myFrame.setVisible(true);
 	}
 }
